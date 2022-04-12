9543da267b3357ceb6d07dff9796333af38a93e2
ammar <ammarraneez@gmail.com>
2022-04-12 17:46:40 +0530

    feat: get voucher details

---
+++ src/organisms/quote/voucher/VoucherTable.tsx
@@ -0,0 +1,153 @@
+import { ChangeEvent, Fragment, useState } from 'react';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import { v4 as uuid } from 'uuid';
+
+import TableColumnCell from '../../../molecules/TableColumnCell';
+import TableRowButtonCell from '../../../molecules/TableRowButtonCell';
+import TableRowIconCell from '../../../molecules/TableRowIconCell';
+import TableRowCheckboxCell from '../../../molecules/TableRowCheckboxCell';
+
+interface VoucherTableProps {
+  columns?: string[];
+  rows?: any[];
+  voucherData: any;
+}
+
+function Row({ row }: any) {
+  const [open, setOpen] = useState(false);
+  const [rowChecked, setRowChecked] = useState(row.status === 'A');
+  const [subTasksChecked, setSubTasksChecked] = useState(
+    row.subtasks
+      ? row.subtasks.map((subtask: any) => subtask.status === 'COMPLETE')
+      : [],
+  );
+
+  const keyboardIcon = open ? (
+    <KeyboardArrowDownIcon />
+  ) : (
+    <KeyboardArrowLeftIcon />
+  );
+
+  const onChangeRowStatus = (e: ChangeEvent<HTMLInputElement>) => {
+    setRowChecked(e.target.checked);
+  };
+
+  const onChangeSubTaskStatus = (
+    e: ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const temp = [...subTasksChecked];
+    temp[index] = e.target.checked;
+    setSubTasksChecked(temp);
+  };
+
+  return (
+    <Fragment>
+      <TableRow>
+        <TableRowCheckboxCell
+          name={row.name}
+          checked={rowChecked}
+          onChange={onChangeRowStatus}
+          align="left"
+        />
+        <TableCell>{row.title}</TableCell>
+        <TableRowButtonCell
+          key={uuid()}
+          onClick={() => null}
+          align="left"
+          btnWidth="8rem"
+          btnSize="medium"
+          btnBorderRadius="0.5rem"
+          btnText={rowChecked ? 'A' : row.status}
+          btnColors={['#29CC97', '#ffffff']}
+          btnDisabled
+        />
+        {row.subtasks ? (
+          <TableRowIconCell
+            align="left"
+            size="small"
+            onClick={() => setOpen(!open)}
+            textColor="#5344C2"
+            padding="0"
+          >
+            {keyboardIcon}
+          </TableRowIconCell>
+        ) : (
+          <TableCell>{''}</TableCell>
+        )}
+      </TableRow>
+      {row.subtasks && (
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box margin={1}>
+                <Table size="small" aria-label="subtasks">
+                  <TableBody>
+                    {row.subtasks.map((subtask: any, index: number) => (
+                      <TableRow key={subtask.id}>
+                        <TableRowCheckboxCell
+                          name={subtask.title}
+                          checked={subTasksChecked[index]}
+                          onChange={(e: ChangeEvent<HTMLInputElement>) => onChangeSubTaskStatus(e, index)}
+                          align="left"
+                        />
+                        <TableRowButtonCell
+                          key={uuid()}
+                          onClick={() => null}
+                          align="left"
+                          btnWidth="8rem"
+                          btnSize="medium"
+                          btnBorderRadius="0.5rem"
+                          btnText={subTasksChecked[index] ? 'COMPLETE' : 'TODO'}
+                          btnColors={
+                            subTasksChecked[index]
+                              ? ['#29CC97', '#ffffff']
+                              : ['#7879F1', '#ffffff']
+                          }
+                          btnDisabled
+                        />
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      )}
+    </Fragment>
+  );
+}
+
+export default function VoucherTable({ columns, rows, voucherData }: VoucherTableProps) {
+  console.log(voucherData);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            {columns!.map((column) => (
+              <TableColumnCell key={uuid()} color="b5b5c3" column={column} />
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows!.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/quote/voucher/Voucher.tsx
+++ src/pages/quote/voucher/Voucher.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { CircularProgress } from '@material-ui/core';
+import { collectionGroup, getDocs } from 'firebase/firestore';
+import _ from 'lodash';
 
+import VoucherTable from '../../../organisms/quote/voucher/VoucherTable';
 import DivAtom from '../../../atoms/DivAtom';
 import { selectWithNavbarHeight } from '../../../redux/containerSizeSlice';
-import { voucherStyles } from '../../../styles';
+import { db } from '../../../firebase';
+import { DASHBOARD_TASK_DATA } from '../../../data';
+import { fetchingDataIndicatorStyles, voucherStyles } from '../../../styles';
 
 function Voucher() {
   const height = useSelector(selectWithNavbarHeight);
+  const [voucherData, setVoucherData] = useState<any>();
+
+  useEffect(() => {
+    const getInitialData = async () => {
+      const vData = (await getDocs(collectionGroup(db, `Vouchers`))).docs;
+      const vouchData = vData.map((dc) => dc.data());
+      const vouchIds = vData.map((dc) => dc.id);
+      vouchIds.forEach((id, i) => {
+        vouchData[i].id = id;
+      });
+
+      const groupedVouchData = _.groupBy(vouchData, (voucher: { quoteNo: string }) => voucher.quoteNo);
+      setVoucherData(groupedVouchData);
+    };
+
+    getInitialData();
+  }, []);
 
   return (
     <DivAtom style={voucherStyles.container}>
@@ -15,7 +39,19 @@ function Voucher() {
           height: `${height}px`,
         }}
       >
-        Voucher
+        {voucherData ? (
+          <DivAtom>
+            <VoucherTable
+              columns={['Voucher ID', 'Quotation Title', 'Status', '']}
+              rows={DASHBOARD_TASK_DATA}
+              voucherData={voucherData}
+            />
+          </DivAtom>
+        ) : (
+          <DivAtom style={fetchingDataIndicatorStyles.container}>
+            <CircularProgress size={20} color="primary" />
+          </DivAtom>
+        )}
       </DivAtom>
     </DivAtom>
   );
