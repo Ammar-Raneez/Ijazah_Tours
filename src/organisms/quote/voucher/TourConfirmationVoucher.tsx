0d28db1f8c01b57095525024e876e608c04cc9d9
ammar <ammarraneez@gmail.com>
2022-04-13 23:05:41 +0530

    feat: tour confirmation voucher

--- src/organisms/quote/voucher/TourConfirmationVoucher.tsx
+++ src/organisms/quote/voucher/TourConfirmationVoucher.tsx
@@ -1,37 +1,183 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import { CircularProgress, TextField } from '@material-ui/core';
+import { doc, setDoc } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import JSPDF from 'jspdf';
+import { v4 as uuid } from 'uuid';
 
 import VoucherSummary from './specific-voucher/VoucherSummary';
 import VoucherGuestTable from './specific-voucher/VoucherGuestTable';
 import Banner from '../quotation/create-quotation/approval/Banner';
+import FormControlInput from '../../../molecules/FormControlInput';
 import DivAtom from '../../../atoms/DivAtom';
 import H2Atom from '../../../atoms/H2Atom';
+import SpanAtom from '../../../atoms/SpanAtom';
+import ButtonAtom from '../../../atoms/ButtonAtom';
+import ParagraphAtom from '../../../atoms/ParagraphAtom';
+import { selectWithNavbarWidth } from '../../../redux/containerSizeSlice';
+import { getElementWidth, uploadPDF, widthHeightDynamicStyle } from '../../../utils/helpers';
+import { db } from '../../../firebase';
+import { TOUR_CONFIRM_STARTING_PROFORMA } from '../../../data';
 import { voucherStyles } from '../../../styles';
+
+const storage = getStorage();
 
 interface TourConfirmationVoucherProps {
   voucherData: any;
+  setIsVoucherApproved: any;
 }
 
-function TourConfirmationVoucher({ voucherData }: TourConfirmationVoucherProps) {
+function TourConfirmationVoucher({ voucherData, setIsVoucherApproved }: TourConfirmationVoucherProps) {
+  const width = useSelector(selectWithNavbarWidth);
+
   const { id } = useParams<{ id: string }>();
   const [quoteNo] = useState(id.split('+')[1]);
   const [vData, setVData] = useState(voucherData[quoteNo].find((voucher: { id: string }) => (
     voucher.id === id.split('+')[0]
   )));
 
-  console.log(vData);
+  const [advance, setAdvance] = useState('');
+  const [balance, setBalance] = useState('');
+  const [proforma, setProforma] = useState(TOUR_CONFIRM_STARTING_PROFORMA);
+
+  const [isSavingVoucher, setIsSavingVoucher] = useState(false);
+
+  const history = useHistory();
+
+  const generatePDF = async () => {
+    const { elementWidth, elementHeight } = getElementWidth('report');
+    const report = new JSPDF('landscape', 'pt', [elementWidth + 10, elementHeight + 10]);
+    return report.html(document.querySelector('#report') as HTMLElement).then(async () => {
+      const filename = `${uuid()}-${vData.guestDetails.name}.pdf`;
+      const pdfURL = await uploadPDF(storage, 'voucher-tour-confirmation-pdfs', report.output('blob'), filename);
+      report.save(filename);
+      return pdfURL;
+    });
+  };
+
+  const saveVoucher = async () => {
+    setIsSavingVoucher(true);
+    setIsVoucherApproved(false);
+    const pdfURL = await generatePDF();
+    const vDataCopy = { ...vData };
+    vDataCopy.pdfURL = pdfURL;
+    vDataCopy.proforma = proforma;
+    vDataCopy.paymentAdvance = advance;
+    vDataCopy.paymentBalance = balance;
+    await updateDB(vDataCopy);
+    setVData(vDataCopy);
+    setIsSavingVoucher(false);
+    setIsVoucherApproved(true);
+    history.replace('/quote/voucher');
+  };
+
+  const updateDB = async (vDataCopy: any) => {
+    const { id: updatedVDataId, ...updatedVData } = vDataCopy;
+    await setDoc(doc(db, 'Vouchers', quoteNo, 'Vouchers', updatedVDataId), updatedVData);
+  };
 
   return (
     <>
-      <H2Atom style={voucherStyles.title} text="Tour Confirmation Voucher" />
+      <H2Atom style={voucherStyles.title} text="Tour Confirmation" />
+      <div id="report">
+        <DivAtom style={{ padding: '2rem' }}>
+          <Banner />
+          <VoucherSummary vData={vData} type="tour-confirmation" />
+          <VoucherGuestTable
+            accColumns={['NIGHTS', 'CITY', 'ACCOMODATION']}
+            guestColumns={['ADULTS', 'CHILDREN', 'AGE']}
+            data={vData}
+          />
+          <DivAtom
+            style={{
+              ...voucherStyles.voucherTemplate.summaryDetails.mainContainer,
+              justifyContent: 'flex-start',
+              flexDirection: 'column',
+            }}
+          >
+            <p style={voucherStyles.voucherTemplate.summaryDetails.detailContainer}>
+              <SpanAtom
+                text="PAYMENT SCHEDULE"
+                style={voucherStyles.voucherTemplate.summaryDetails.label}
+              />
+            </p>
+            <FormControlInput
+              width={widthHeightDynamicStyle(width, 1300, '50%', '30%') as string}
+              label="Advance"
+              fullWidth={false}
+              multiline={false}
+              rows={1}
+              value={advance}
+              setValue={setAdvance}
+              placeholder=""
+            />
+            <FormControlInput
+              width={widthHeightDynamicStyle(width, 1300, '50%', '30%') as string}
+              label="Balance"
+              fullWidth={false}
+              multiline={false}
+              rows={1}
+              value={balance}
+              setValue={setBalance}
+              placeholder=""
+            />
+            <p
+              style={{
+                ...voucherStyles.voucherTemplate.summaryDetails.detailContainer,
+                width: '30%',
+                marginTop: '1rem',
+              }}
+            >
+              <SpanAtom
+                text="Net Price"
+                style={voucherStyles.voucherTemplate.summaryDetails.label}
+              />
+              <SpanAtom
+                text={vData.guestDetails.netPrice}
+                style={voucherStyles.voucherTemplate.summaryDetails.detail}
+              />
+            </p>
+          </DivAtom>
+          <DivAtom
+            style={{
+              ...voucherStyles.voucherTemplate.summaryDetails.mainContainer,
+              borderBottom: 0,
+            }}
+          >
+            {!isSavingVoucher ? (
+              <TextField
+                multiline
+                fullWidth
+                variant="outlined"
+                maxRows={20}
+                minRows={5}
+                value={proforma}
+                onChange={(e) => setProforma(e.target.value)}
+                label="Proforma"
+                color="primary"
+                focused
+              />
+            ) : (
+              <ParagraphAtom text={proforma} />
+            )}
+          </DivAtom>
+        </DivAtom>
+      </div>
+
       <DivAtom style={{ padding: '2rem' }}>
-        <Banner />
-        <VoucherSummary vData={vData} type="tour-confirmation" />
-        <VoucherGuestTable
-          accColumns={['NIGHTS', 'CITY', 'ACCOMODATION']}
-          guestColumns={['ADULTS', 'CHILDREN', 'AGE']}
-          data={vData}
+        <ButtonAtom
+          size="large"
+          text="Confirm"
+          endIcon={isSavingVoucher && <CircularProgress size={20} color="inherit" />}
+          disabled={isSavingVoucher || advance === '' || balance === '' || proforma === ''}
+          onClick={saveVoucher}
+          style={{
+            ...voucherStyles.addBtn,
+            width: widthHeightDynamicStyle(width, 768, '100%', '15%'),
+            margin: widthHeightDynamicStyle(width, 768, '100%', '18%') ? '0 1rem 1rem 0' : '0 0 1rem 2rem',
+          }}
         />
       </DivAtom>
     </>
